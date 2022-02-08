69c4e4b1147d31b5a3727f7be08ae35e94b87756
update <timife007@gmail.com>
2022-02-08 13:11:59 -0500

    api correct

--- app/src/main/java/com/timife/teesast/utils/Constants.kt
+++ app/src/main/java/com/timife/teesast/utils/Constants.kt
@@ -2,5 +2,8 @@ package com.timife.teesast.utils
 
 object Constants {
     const val BASE_ENDPOINT = "https://run.mocky.io/v3/"
-    const val AUTH_ENDPOINT = "1a2eae07-997f-4edc-a756-563e3e4536e4"
+    const val AUTH_ENDPOINT = "50536a34-b4f2-4942-9142-97d506645862"
 }
+
+//    https://run.mocky.io/v3/50536a34-b4f2-4942-9142-97d506645862
+//1a2eae07-997f-4edc-a756-563e3e4536e4
